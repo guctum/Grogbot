42cc1d4f4a8347a447e3cebc8ec2e4387dff7f9b
Greg Uctum <greguctum@gmail.com>
2020-12-22 17:59:55 -0500

    bot works with joke function, also has Brooklyn 99 stuff from followed tutorial and to verify that this can handle multiple commands

---
+++ bot.py
@@ -0,0 +1,42 @@
+import os
+import random
+import requests
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '!99':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+    if message.content == '!joke':
+        url = "https://icanhazdadjoke.com"
+        headers = {'Accept': 'application/json'}
+        response = requests.get(url, headers=headers).json().get('joke')
+        await message.channel.send(response)
+
+
+client.run(TOKEN)
