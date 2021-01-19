70c4ea4d4419d72644ded38cbf66750f828036a5
Greg Uctum <greguctum@gmail.com>
2021-01-18 21:05:40 -0500

    scraper working, need to format it for discord

--- scraper.py
+++ scraper.py
@@ -1,12 +1,29 @@
 import requests
-import discord
 from bs4 import BeautifulSoup
+from itertools import islice
+
+url = 'https://store.steampowered.com/search/?filter=topsellers&tags=19'
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+}
+response = requests.get(url, headers=header)
 
 def get_action_games():
     print("Retrieving action games")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    names = []
+    for x in soup.find_all("span", {"class": "title"}):
+        names.append(x.get_text())
+    prices = []
+    for y in soup.find_all("div", {"class": "search_price"}):
+        prices.append((y.get_text()).replace("\r\n", "").strip())
+    res = {}
+    for name in names:
+        for price in prices:
+            res[name] = price
+            prices.remove(price)
+            break
 
-def get_rpgs():
-    print("Retrieving RPG games")
+    n_items = list(islice(res.items(), 10))
 
-def get_multiplayer_shooters():
-    print("Retrieving multiplayer shooters")
+    print(n_items)
